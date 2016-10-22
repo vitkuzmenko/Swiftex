a3f4e4f910e88ded92212806677d82438f289467
vitkuzmenko <kuzmenko.v.u@gmail.com>
2016-10-23 00:36:18 +0300

    keyboard notifications moved to nsobject

--- Extensions/Foundation/NSObject+Extension.swift
+++ Extensions/Foundation/NSObject+Extension.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Wallet One. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension NSObject {
     
@@ -16,6 +16,92 @@ extension NSObject {
     
     public var nameOfClass: String {
         return NSStringFromClass(type(of: self)).components(separatedBy: ".").last!
+    }
+    
+    // MARK: - Keyboard
+    
+    open func addKeyboardWillShowNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    open func addKeyboardDidShowNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: .UIKeyboardDidShow, object: nil)
+    }
+    
+    open func addKeyboardWillHideNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    open func addKeyboardDidHideNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(notification:)), name: .UIKeyboardDidHide, object: nil)
+    }
+    
+    open func removeKeyboardWillShowNotification() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+    }
+    
+    open func removeKeyboardDidShowNotification() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardDidShow, object: nil)
+    }
+    
+    open func removeKeyboardWillHideNotification() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    open func removeKeyboardDidHideNotification() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardDidHide, object: nil)
+    }
+    
+    open func keyboardDidShow(notification: Notification) {
+        let nInfo = notification.userInfo as! [String: NSValue]
+        let value = nInfo[UIKeyboardFrameEndUserInfoKey]
+        
+        if let frame = value?.cgRectValue {
+            keyboardDidShow(frame: frame)
+        }
+    }
+    
+    open func keyboardWillShow(notification: Notification) {
+        let nInfo = notification.userInfo as! [String: NSValue]
+        let value = nInfo[UIKeyboardFrameEndUserInfoKey]
+        
+        if let frame = value?.cgRectValue {
+            keyboardWillShow(frame: frame)
+        }
+    }
+    
+    open func keyboardWillHide(notification: Notification) {
+        let nInfo = notification.userInfo as! [String: NSValue]
+        let value = nInfo[UIKeyboardFrameEndUserInfoKey]
+        
+        if let frame = value?.cgRectValue {
+            keyboardWillHide(frame: frame)
+        }
+    }
+    
+    open func keyboardDidHide(notification: Notification) {
+        let nInfo = notification.userInfo as! [String: NSValue]
+        let value = nInfo[UIKeyboardFrameEndUserInfoKey]
+        
+        if let frame = value?.cgRectValue {
+            keyboardDidHide(frame: frame)
+        }
+    }
+    
+    open func keyboardWillShow(frame: CGRect) {
+        
+    }
+    
+    open func keyboardDidShow(frame: CGRect) {
+        
+    }
+    
+    open func keyboardWillHide(frame: CGRect) {
+        
+    }
+    
+    open func keyboardDidHide(frame: CGRect) {
+        
     }
     
 }
