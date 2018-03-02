7a93e6d84bf4a45e9bbf8be7ccee36877d666487
vitkuzmenko <kuzmenko.v.u@gmail.com>
2018-03-02 14:23:26 +0300

    tv

--- Extensions/UIKit/UIView+Extensions.swift
+++ Extensions/UIKit/UIView+Extensions.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 KuzmenkoFamily. All rights reserved.
 //
 
-#if os(iOS) || os(watchOS)
+//#if os(iOS) || os(watchOS)
 
 import UIKit
 
@@ -44,5 +44,5 @@ extension UIView {
     
 }
 
-#endif
+//#endif
 
